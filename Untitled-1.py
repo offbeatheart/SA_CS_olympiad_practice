ea4df28e11cc53d6a34aea0b4ebacb0bfa56f760
offbeatheartds <offbeatheartds@gmail.com>
2026-07-07 07:56:47 +0200

    tested the limits the viablity of different o complexites given a certainly large n

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,11 @@
-for num in range(1000,10000):
-    number = str(num)
-    reverse_flash = number[3] + number[2] + number[1] + number[0] 
-    if (num * 4) == int(reverse_flash):
-        print(num)
+# for num in range(1000,10000):
+#     number = str(num)
+#     reverse_flash = number[3] + number[2] + number[1] + number[0] 
+#     if (num * 4) == int(reverse_flash):
+#         print(num)
+total =0
+n = int(input())
+for num in range(0,n):
+    total += 1
+
+print(total)
