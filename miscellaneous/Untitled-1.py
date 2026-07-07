fd8e226cf6d0cd79246e53ccdbc22c162a5f8a1b
offbeatheartds <offbeatheartds@gmail.com>
2026-07-07 15:01:16 +0200

    create miscellaneous file

--- miscellaneous/Untitled-1.py
+++ miscellaneous/Untitled-1.py
@@ -24,3 +24,5 @@ print(p1[0])  # access the first element of the tuple
 
 p2 = (6, "asdf")
 print(p1 < p2)
+
+my_pair = (0,2,3)
---
+++ miscellaneous/naive_fibinaci.py
@@ -0,0 +1,21 @@
+n = int(input())
+nfib =[0,1]
+
+while len(nfib) < n +1:
+    nfib.append(nfib[-2] + nfib[-1])
+
+if n == 0:
+    print(0)
+else:
+    print(nfib[-1])
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n -1) + fib(n - 2)
+
+print(fib(5))
