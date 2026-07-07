026f22c240d9d9d19bd6f6006917de94b067110f
offbeatheartds <offbeatheartds@gmail.com>
2026-07-07 07:59:33 +0200

    messing around with pairs

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,9 +3,24 @@
 #     reverse_flash = number[3] + number[2] + number[1] + number[0] 
 #     if (num * 4) == int(reverse_flash):
 #         print(num)
-total =0
-n = int(input())
-for num in range(0,n):
-    total += 1
 
-print(total)
+# total =0
+# n = int(input())
+# for num in range(0,n):
+#     total += 1
+
+# print(total)
+
+"""
+Output:
+(5, 'asdf')
+5
+True
+"""
+
+p1 = (6, "asdfasd")
+print(p1)
+print(p1[0])  # access the first element of the tuple
+
+p2 = (6, "asdf")
+print(p1 < p2)
