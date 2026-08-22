4228b7aacc1095e6b89fdc45a3b1ce5eff09ece5
offbeatheartds <offbeatheartds@gmail.com>
2026-08-22 12:54:13 +0200

    finished Q1 ahhhhhhhhhhhhh

---
+++ 2025/round 2/rectangle.py
@@ -0,0 +1,108 @@
+R,C = map(int,input().split())
+
+whole = []
+
+def template_gen(R,C):
+    global whole
+    temp = []
+    for item in range(C):
+        temp.append("a")
+
+    for item in range(R):
+        whole.append(temp.copy())
+
+seq = []
+
+
+template_gen(R,C)
+
+for value in range(1,(R * C) + 1):
+
+    temp = []
+
+    if (value %(C - 1)) == 0:
+        temp.append(value)
+        # print((value % C)-1)
+        # print(value // R)
+        seq.append(value)
+
+index = 1 
+
+def pretty_printing():
+    for item in whole:
+        print(item)
+
+def indices():
+    global whole
+    final = []
+    index = -1
+    prev = (seq[0]-1)%C
+    total = 0
+    for item in seq:
+        
+        value = (item-1)%C
+
+        final.append(value)
+        if prev >= value:
+            index += 1
+            
+        prev = value
+
+        total += whole[index][value]
+        # print(whole[index][value])
+
+    print(total)
+
+
+
+
+def right(shift):
+    global index, whole
+    for item in range(0,len(whole[0])):
+        if whole[0 + shift][item] == 'a':
+            whole[0 + shift][item] = index
+            index += 1 
+
+def down(shift):
+    global index, whole
+    for item in range(0,len(whole)):
+        if whole[item][-1 - shift] == 'a':
+            whole[item][-1 - shift] = index
+            index += 1 
+
+def left(shift):
+    global index, whole
+    for item in range(len(whole[0]) -1 ,-1,-1):
+        if whole[-1 - shift][item] == 'a':
+            whole[-1 - shift][item] = index
+            index += 1 
+
+def up(shift):
+    global index, whole
+    for item in range(len(whole) -1 ,-1,-1):
+        if whole[item][0 + shift] == 'a':
+            whole[item][0+ shift] = index
+            index += 1 
+
+shift = 0
+while True:
+    try:
+        right(shift)
+    except:
+        break
+    try:
+        down(shift)
+    except:
+        break
+    try:
+        left(shift)
+    except:
+        break
+    try:
+        up(shift)
+    except:
+        break
+
+    shift += 1
+
+indices()
