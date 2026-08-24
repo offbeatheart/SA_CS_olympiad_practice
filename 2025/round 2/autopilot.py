c074e9e0154f481bfb7de9d96c12f3c28ac531ac
offbeatheartds <offbeatheartds@gmail.com>
2026-08-24 14:13:50 +0200

    implemented the solution with hash maps to reduce the time complexity so it runs on time

--- 2025/round 2/autopilot.py
+++ 2025/round 2/autopilot.py
@@ -1,60 +1,29 @@
 moves = list(input())
 # moves = ['L','U','R','D']
-past = [[0,0]]
+past = {(0,0)}
 
 
 flag = False
+x =0
+y =0
 for move in moves:
-    head = past[-1].copy()
 
     if move == 'U':
-        head[1] += 1
+        y += 1
     elif move == "D":
-        head[1] -= 1
+        y -= 1
     if move == 'R':
-        head[0] += 1
+        x += 1
     elif move == "L":
-        head[0] -= 1
+        x -= 1
 
-    if head in past:
+    if (x,y) in past:
         flag = True
         break
     else:
-        past.append(head)
+        past.add((x,y))
 
 if flag:
     print(len(past))
 else:
     print(-1)
-
-
-# flag = False
-# pos= [0,0]
-# for move in moves:
-#     temp = []
-#     if move == 'U':
-#         temp = [pos[0], pos[1]+ 1]
-#         pos[1] += 1
-        
-#     elif move == 'D':
-#         temp = [pos[0], pos[1]- 1]
-#         pos[1] -= 1
-#     elif move == 'L':
-#         temp = [pos[0]+1, pos[1]]
-#         pos[0] -= 1
-#     elif move == 'R':
-#         temp = [pos[0]-1, pos[1]]
-#         pos[0] -= 1
-
-#     if pos in past:
-#         flag = True 
-#         break
-#     elif move == ' ':
-#         pass
-#     else:
-#         past.append(temp)
-
-# if flag:
-#     print(len(past)-1)
-# else:
-#     print(-1)
